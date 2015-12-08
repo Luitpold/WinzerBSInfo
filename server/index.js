f169b55327ed5e74269f6018fc02ea0a90a6d8e0
Ruben H <horn.ruben@yahoo.de>
2015-12-08 19:24:03 +0100

    minor changes and comments
    minor changes and comments

--- server/index.js
+++ server/index.js
@@ -1,29 +1,32 @@
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
+var fs = require('fs'); //import the file handling module
+var http = require('http'); //import http module
+var https = require('https'); //import https module
 
-var colors = require('colors'); //Make the console output look nicer
+var colors = require('colors'); var clear = require('clear'); clear(); //Make the console output look nicer
 
 var useSsl = false;
 
+//setup web server
 var express = require('express');
 var app = express();
 
 var httpServer = http.createServer(app);
 
+//start web server
 function init(httpPort, httpsPort){
+  //setup additional styles for the console
   colors.setTheme({
     error: ['white', 'bgRed']
   });
 
   console.log(getStatusPrefix() + '\tstarting http-Server..');
-  httpServer.listen(httpPort);
+  httpServer.listen(httpPort); //start http server
   console.log(getStatusPrefix() + '\thttp-Servers running!\n' + getStatusPrefix() + '\tstarting https-Server..');
-  startHttpsServer();
+  startHttpsServer(); //start https server
 }
 
 function startHttpsServer(){
-  try{
+  try{ //check if the credentials are provided (/ssl/server.key; /ssl/server.cert)
     var privateKey = fs.readFileSync(__dirname + '/../ssl/server.key', 'utf8');
     var certificate = fs.readFileSync(__dirname + '/../ssl/server.cert', 'utf8');
 
@@ -31,8 +34,8 @@ function startHttpsServer(){
     var httpsServer = https.createServer(credential, app);
     httpsServer.listen(httpsPort);
     console.log('STATUS:'.blue + '\thttps-Servers running!');
-    useSsl = true;
-  }catch(e){
+    useSsl = true; //enable https enforcement
+  }catch(e){ //show error in console
     var err = ('' + e).error;
     console.log(getStatusPrefix() + '\thttps-Server was not created!\n'+ err);
   }
@@ -49,10 +52,10 @@ function getDate(){
 
 //force ssl for maximum security
 app.use(function(req, res, next) {
-   if(!req.secure && useSsl) {
+   if(!req.secure && useSsl) { //redirect to https if http is used and https server is running
        return res.redirect(['https://', req.get('Host'), req.url].join(''));
    }
-   console.log((getDate() + ' TRAFFIC:').green + '\t"' + (req.url).gray + '" was requested.');
+   console.log((getDate() + ' TRAFFIC:').green + '\t"' + (req.url).gray + '" was requested by "' + (req.ip + '').gray + '".');
    next();
 });
 
